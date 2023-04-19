130eb729c7374db26317118cbc2dccc0807a4a65
Laszlo E Kovacs <laszloekovacs@gmail.com>
2023-04-19 21:19:21 +0200

    added aiming point

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,19 +1,27 @@
 import { Viewer } from '@photo-sphere-viewer/core'
 import React, { useEffect, useRef, useState } from 'react'
-
+import '../overrides.css'
 const Preview = ({ data }) => {
 	const viewRef = useRef<Viewer | null>(null)
 	const divRef = useRef<HTMLDivElement | null>(null)
 
-	useEffect(() => {
-		if (!data) {
-			console.log('no data')
-			return
-		}
+	if (!data) {
+		console.log('no data')
+		return
+	}
 
+	useEffect(() => {
+		/* creation */
 		const container = { container: divRef.current as HTMLElement }
 		viewRef.current = new Viewer({ ...container, ...data })
 
+		/* add aim point, make it permanent */
+		viewRef.current.overlay.show({
+			title: '',
+			dissmisable: false
+		})
+
+		/* cleanup */
 		return () => {
 			viewRef.current?.destroy()
 		}
@@ -24,7 +32,7 @@ const Preview = ({ data }) => {
 		<div
 			id='view'
 			ref={divRef}
-			className='-z-50 h-full w-full border border-neutral'></div>
+			className='isolate -z-50 h-full w-full border border-neutral'></div>
 	)
 }
 
